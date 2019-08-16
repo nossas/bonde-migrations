e0452800a9a862fb4985309bc4170210af7ec17f
Lucas Pirola <lucaspirola@gmail.com>
2019-08-16 14:40:01 -0300

    chore(chatbot): Update campaigns table;
    Add drop role sqls to initial down migration;

--- migrations/2018-07-31-182711_initial/down.sql
+++ migrations/2018-07-31-182711_initial/down.sql
@@ -4,3 +4,10 @@ DROP SCHEMA microservices;
 DROP SCHEMA pgjwt;
 DROP SCHEMA postgraphile_watch;
 DROP SCHEMA postgraphql;
+drop role postgraphql;
+drop role anonymous;
+drop role common_user;
+drop role admin;
+drop role postgres;
+drop role microservices;
+
