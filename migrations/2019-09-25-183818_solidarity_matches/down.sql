8309b7ad52be626e5b836c8e8259f36d368b3670
Gabriel Rocha de Oliveira <rolivegab@localhost.localdomain>
2019-09-25 17:38:32 -0300

    chore: rename solidarity_matches

--- migrations/2019-09-25-183818_solidarity_matches/down.sql
+++ migrations/2019-09-25-183818_solidarity_matches/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE solidarity_match;
+DROP TABLE solidarity_matches;
--- migrations/2019-09-25-183818_solidarity_matches/up.sql
+++ migrations/2019-09-25-183818_solidarity_matches/up.sql
@@ -1,5 +1,5 @@
 -- Your SQL goes here
-CREATE TABLE solidarity_match (
+CREATE TABLE solidarity_matches (
     id SERIAL PRIMARY KEY,
     msr_ticket INTEGER REFERENCES solidarity_zd_tickets(id),
     voluntaria_ticket INTEGER REFERENCES solidarity_zd_tickets(id),
