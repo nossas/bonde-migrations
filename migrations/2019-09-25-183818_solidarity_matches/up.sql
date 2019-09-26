fe585ba0d498835165fd5915bdede413c2078e9b
Gabriel Rocha de Oliveira <rolivegab@localhost.localdomain>
2019-09-25 22:54:05 -0300

    feat: add community_id to solidarity_matches

--- migrations/2019-09-25-183818_solidarity_matches/up.sql
+++ migrations/2019-09-25-183818_solidarity_matches/up.sql
@@ -5,6 +5,7 @@ CREATE TABLE solidarity_matches (
     volunteers_ticket_id INTEGER REFERENCES solidarity_tickets(id),
     individuals_user_id INTEGER REFERENCES solidarity_users(id),
     volunteers_user_id INTEGER REFERENCES solidarity_users(id),
+    community_id INTEGER REFERENCES communities(id),
     UNIQUE(individuals_ticket_id),
     UNIQUE(volunteers_ticket_id),
     UNIQUE(individuals_user_id, volunteers_user_id)
