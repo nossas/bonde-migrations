23c8a31261550a6780dc1702a4523b6c464265f0
Igor Santos <igr.exe@gmail.com>
2019-10-04 17:13:07 -0300

    fix(chatbot): remove prefix field on campaigns

---
+++ migrations/2019-10-04-160931_delete_chatbot_campaigns_prefix/down.sql
@@ -0,0 +1,4 @@
+-- Your SQL goes here
+
+-- drop prefix column on chatbot_campaigns table
+alter table chatbot_campaigns add column prefix boolean;
---
+++ migrations/2019-10-04-160931_delete_chatbot_campaigns_prefix/up.sql
@@ -0,0 +1,4 @@
+-- Your SQL goes here
+
+-- drop prefix column on chatbot_caimpaigns table
+alter table chatbot_campaigns drop column prefix cascade;
