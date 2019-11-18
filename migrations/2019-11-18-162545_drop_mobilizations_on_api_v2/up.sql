d4f4bffd491c8e1c8c47918cea24be5d599a21f6
Igor Santos <igr.exe@gmail.com>
2019-11-18 16:46:38 -0300

    feat: remove dependencies mobilizations of api-v2

---
+++ migrations/2019-11-18-162545_drop_mobilizations_on_api_v2/up.sql
@@ -0,0 +1,23 @@
+/*ERROR: cannot drop view postgraphql.mobilizations because other objects depend on it DETAIL: view postgraphql.user_mobilizations depends on view postgraphql.mobilizations function postgraphql.user_mobilizations_community(postgraphql.user_mobilizations) depends on type postgraphql.user_mobilizations function postgraphql.user_mobilizations_score(postgraphql.user_mobilizations) depends on type postgraphql.user_mobilizations function postgraphql.mobilizations_community(postgraphql.mobilizations) depends on type postgraphql.mobilizations function postgraphql.trending_mobilizations(integer) depends on type postgraphql.mobilizations HINT: Use DROP ... CASCADE to drop the dependent objects too. */
+
+-- Function: postgraphql.user_mobilizations_community
+DROP FUNCTION postgraphql.user_mobilizations_community(postgraphql.user_mobilizations);
+
+-- Function: postgraphql.user_mobilizations_score(postgraphql.user_mobilizations)
+
+DROP FUNCTION postgraphql.user_mobilizations_score(postgraphql.user_mobilizations);
+
+-- View: postgraphql.user_mobilizations
+DROP VIEW postgraphql.user_mobilizations;
+
+-- Function: postgraphql.trending_mobilizations(integer)
+DROP FUNCTION postgraphql.trending_mobilizations(integer);
+
+-- Function: postgraphql.mobilizations_community(postgraphql.mobilizations)
+DROP FUNCTION postgraphql.mobilizations_community(postgraphql.mobilizations);
+
+-- View: postgraphql.mobilizations
+DROP VIEW postgraphql.mobilizations;
+
+-- Function: postgraphql.mobilizations(integer)
+DROP FUNCTION postgraphql.mobilizations(integer);
