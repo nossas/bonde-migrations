ca0cf83eb4affa24b466be9e200c5d3e806ee4ca
Igor Santos <igr.exe@gmail.com>
2019-11-29 17:35:20 -0300

    chore: remove unused functions to reset password

---
+++ migrations/2019-11-28-171523_remove_reset_password_functions_on_api_v2/up.sql
@@ -0,0 +1,5 @@
+DROP FUNCTION IF EXISTS postgraphql.reset_password_change_password(text, text);
+
+DROP FUNCTION IF EXISTS postgraphql.reset_password_token_request(text, text, text);
+
+DROP FUNCTION IF EXISTS postgraphql.reset_password_token_verify(text);
