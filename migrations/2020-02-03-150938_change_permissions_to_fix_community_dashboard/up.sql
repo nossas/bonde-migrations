5e3d9d93de393cd649960040d7f2f5cdd51a9457
Igor Santos <igr.exe@gmail.com>
2020-02-03 15:31:51 -0300

    fix: change perms to community dashboad

---
+++ migrations/2020-02-03-150938_change_permissions_to_fix_community_dashboard/up.sql
@@ -0,0 +1,5 @@
+GRANT SELECT ON TABLE postgraphql.participations TO admin;
+GRANT SELECT ON TABLE postgraphql.participations TO common_user;
+
+GRANT SELECT ON TABLE postgraphql.donations TO admin;
+GRANT SELECT ON TABLE postgraphql.donations TO common_user;
