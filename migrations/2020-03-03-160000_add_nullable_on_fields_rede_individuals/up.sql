2668b6c3a9f43a2440364493eebf549e6a59e30b
Igor Santos <igr.exe@gmail.com>
2020-03-03 16:12:45 -0300

    fix(redes): become some fields nullable on rede_individuals

---
+++ migrations/2020-03-03-160000_add_nullable_on_fields_rede_individuals/up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE public.rede_individuals ALTER COLUMN register_occupation DROP NOT NULL;
+ALTER TABLE public.rede_individuals ALTER COLUMN field_occupation DROP NOT NULL;
+ALTER TABLE public.rede_individuals ALTER COLUMN state DROP NOT NULL;
+ALTER TABLE public.rede_individuals ALTER COLUMN city DROP NOT NULL;
+ALTER TABLE public.rede_individuals ALTER COLUMN latitude DROP NOT NULL;
+ALTER TABLE public.rede_individuals ALTER COLUMN longitude DROP NOT NULL;
