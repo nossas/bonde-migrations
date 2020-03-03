a1b225235dfbc3bd235bd7ca83d2cb24b102f20e
Igor Santos <igr.exe@gmail.com>
2020-03-03 16:11:48 -0300

    fix(redes): split name

---
+++ migrations/2020-03-03-154737_split_name_on_rede_individuals/down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.rede_individuals ADD COLUMN name VARCHAR NOT NULL;
+
+ALTER TABLE public.rede_individuals DROP COLUMN first_name;
+
+ALTER TABLE public.rede_individuals DROP COLUMN last_name;
---
+++ migrations/2020-03-03-154737_split_name_on_rede_individuals/up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.rede_individuals DROP COLUMN name;
+
+ALTER TABLE public.rede_individuals ADD COLUMN first_name VARCHAR NOT NULL;
+
+ALTER TABLE public.rede_individuals ADD COLUMN last_name VARCHAR NOT NULL;
