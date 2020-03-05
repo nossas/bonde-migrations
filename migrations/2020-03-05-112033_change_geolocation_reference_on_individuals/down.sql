5171b1bf88d54f71cae0f615e33d74d22296bc30
Igor Santos <igr.exe@gmail.com>
2020-03-05 11:51:21 -0300

    feat(redes): add zipcode and coordinates on individuals

---
+++ migrations/2020-03-05-112033_change_geolocation_reference_on_individuals/down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE public.rede_individuals DROP COLUMN coordinates JSONB;
+
+ALTER TABLE public.rede_individuals ADD COLUMN latitude VARCHAR;
+ALTER TABLE public.rede_individuals ADD COLUMN longitude VARCHAR;
+
+ALTER TABLE public.rede_individuals DROP COLUMN zipcode;
---
+++ migrations/2020-03-05-112033_change_geolocation_reference_on_individuals/up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE public.rede_individuals ADD COLUMN coordinates JSONB;
+
+ALTER TABLE public.rede_individuals DROP COLUMN latitude;
+ALTER TABLE public.rede_individuals DROP COLUMN longitude;
+
+ALTER TABLE public.rede_individuals ADD COLUMN zipcode VARCHAR(8) NOT NULL;
