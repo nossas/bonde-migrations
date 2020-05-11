6b7f7c3e875fd5c8fd0b37a4dcdd461e07ce0066
Igor Santos <igr.exe@gmail.com>
2020-05-11 11:01:36 -0300

    fix(donations): change json parse to hstore on stats donation

---
+++ migrations/2020-05-11-105834_change_get_widget_donation_stats/up.sql
@@ -0,0 +1,39 @@
+-- FUNCTION: postgraphql.get_widget_donation_stats(integer)
+
+-- DROP FUNCTION postgraphql.get_widget_donation_stats(integer);
+
+CREATE OR REPLACE FUNCTION postgraphql.get_widget_donation_stats(
+	widget_id integer)
+    RETURNS json
+    LANGUAGE 'sql'
+
+    COST 100
+    STABLE 
+AS $BODY$
+        select
+            json_build_object(
+            'pledged', sum(d.amount / 100) + coalesce(nullif(w.settings::hstore->'external_resource', ''), '0')::bigint,
+            'widget_id', w.id,
+            'goal', w.goal,
+            'progress', ((sum(d.amount / 100) + coalesce(nullif(w.settings::hstore->'external_resource', ''), '0')::bigint) / w.goal) * 100,
+            'total_donations', (count(distinct d.id)),
+            'total_donators', (count(distinct d.activist_id))
+            )
+        from widgets w
+            join donations d on d.widget_id = w.id
+            where w.id = $1 and
+                d.transaction_status = 'paid'
+            group by w.id;
+        $BODY$;
+
+ALTER FUNCTION postgraphql.get_widget_donation_stats(integer)
+    OWNER TO reboo;
+
+GRANT EXECUTE ON FUNCTION postgraphql.get_widget_donation_stats(integer) TO anonymous;
+
+GRANT EXECUTE ON FUNCTION postgraphql.get_widget_donation_stats(integer) TO PUBLIC;
+
+GRANT EXECUTE ON FUNCTION postgraphql.get_widget_donation_stats(integer) TO reboo;
+
+COMMENT ON FUNCTION postgraphql.get_widget_donation_stats(integer)
+    IS 'Returns a json with pledged, progress and goal from widget';
