e9d60c6b741d2afa89ee19a880df6e5d9922aaab
Igor Santos <igr.exe@gmail.com>
2020-06-05 11:39:58 -0300

    feat: add is_admin to identify superusers

---
+++ migrations/2020-06-05-113600_add_is_admin_on_users/up.sql
@@ -0,0 +1,36 @@
+ALTER TABLE public.users ADD COLUMN is_admin BOOLEAN DEFAULT false;
+
+CREATE OR REPLACE VIEW postgraphql.users
+ AS
+ SELECT u.id,
+    u.provider,
+    u.uid,
+    u.encrypted_password,
+    u.reset_password_token,
+    u.reset_password_sent_at,
+    u.remember_created_at,
+    u.sign_in_count,
+    u.current_sign_in_at,
+    u.last_sign_in_at,
+    u.current_sign_in_ip,
+    u.last_sign_in_ip,
+    u.confirmation_token,
+    u.confirmed_at,
+    u.confirmation_sent_at,
+    u.unconfirmed_email,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.tokens,
+    u.created_at,
+    u.updated_at,
+    u.avatar,
+    u.admin,
+    u.locale,
+    COALESCE(json_agg(t.name), '[]'::json) AS tags,
+	  u.is_admin
+   FROM users u
+     LEFT JOIN user_tags ut ON ut.user_id = u.id
+     LEFT JOIN tags t ON t.id = ut.tag_id
+  WHERE u.id = current_setting('jwt.claims.user_id'::text)::integer
+  GROUP BY u.id;
